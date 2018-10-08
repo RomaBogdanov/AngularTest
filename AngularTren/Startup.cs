e94bc75e90098128305e354663e3f1f1d62c0d81
rbogdanov <superdiadaroma@rambler.ru>
2018-10-08 20:31:06 +0300

    Строка подключения вынесена в конфигурационный файл

--- AngularTren/Startup.cs
+++ AngularTren/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using AngularTren.Models;
 using Microsoft.EntityFrameworkCore;
@@ -9,10 +10,17 @@ namespace AngularTren
 {
     public class Startup
     {
+        public IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=employeesdb;Trusted_Connection=True;";
-            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+            //string connectionString = "Server=(localdb)\\mssqllocaldb;Database=employeesdb;Trusted_Connection=True;";
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc();
         }
